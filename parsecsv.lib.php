a40e6711766f5082a0cf8eb2fe4470c6954a94fe
Евгений Грицацуев <zapavednik@yandex.com>
2017-08-20 14:46:34 +0300

    Update parsecsv.lib.php

--- parsecsv.lib.php
+++ parsecsv.lib.php
@@ -920,7 +920,7 @@ class parseCSV {
             {
                 if('' === $this->input_encoding)
                 {
-                    $encoding = mb_detect_encoding( $input, array(
+                    $this->input_encoding = mb_detect_encoding( $input, array(
                         'ASCII',
                         'Windows-1251','Windows-1252','Windows-1254','Windows-1257',
                         'ISO-8859-1','ISO-8859-2','ISO-8859-3','ISO-8859-4','ISO-8859-5','ISO-8859-6','ISO-8859-7',
@@ -928,10 +928,7 @@ class parseCSV {
                         'UTF-8','UTF-32','UTF-32BE','UTF-32LE','UTF-16','UTF-16BE','UTF-16LE'
                         ), TRUE );
 
-                    if(FALSE === $encoding)
-                        $this->input_encoding = 'ISO-8859-1';
-                    else
-                        $this->input_encoding = $encoding;
+                    if(FALSE === $this->input_encoding) $this->input_encoding = 'ISO-8859-1';
                 }
                             
                 $substchar = ini_get('mbstring.substitute_character');
