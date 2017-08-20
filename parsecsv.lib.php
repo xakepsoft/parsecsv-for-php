ee79e7cb5e10a9b1503d26eac835122f4a35f9ce
Евгений Грицацуев <zapavednik@yandex.com>
2017-08-20 13:56:51 +0300

    Update parsecsv.lib.php

--- parsecsv.lib.php
+++ parsecsv.lib.php
@@ -915,9 +915,25 @@ class parseCSV {
                 $data = substr($data, 2);
                 if('' === $this->input_encoding || 'ISO-8859-1' === $this->input_encoding) $this->input_encoding = 'UTF-16LE';
             }
+            
+            if ($this->convert_encoding)
+            {
+                if('' === $this->input_encoding)
+                {
+                    $encoding = mb_detect_encoding( $input, array(
+                        'ASCII',
+                        'Windows-1251','Windows-1252','Windows-1254','Windows-1257',
+                        'ISO-8859-1','ISO-8859-2','ISO-8859-3','ISO-8859-4','ISO-8859-5','ISO-8859-6','ISO-8859-7',
+                        'ISO-8859-8','ISO-8859-9','ISO-8859-10','ISO-8859-13','ISO-8859-14','ISO-8859-15','ISO-8859-16',
+                        'UTF-8','UTF-32','UTF-32BE','UTF-32LE','UTF-16','UTF-16BE','UTF-16LE'
+                        ), TRUE );
 
-            if ($this->convert_encoding) {
-                //$data = iconv($this->input_encoding, $this->output_encoding, $data);
+                    if(FALSE === $encoding)
+                        $this->input_encoding = 'ISO-8859-1';
+                    else
+                        $this->input_encoding = $encoding;
+                }
+                            
                 $substchar = ini_get('mbstring.substitute_character');
                 ini_set('mbstring.substitute_character', "none");
                 $data = mb_convert_encoding($data, $this->output_encoding, $this->input_encoding);
