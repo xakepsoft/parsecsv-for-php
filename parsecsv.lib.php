b55d69b6b1bb1acbe02137138db8b7a8dd4c1288
Евгений Грицацуев <zapavednik@yandex.com>
2017-08-20 16:13:07 +0300

    Update parsecsv.lib.php

--- parsecsv.lib.php
+++ parsecsv.lib.php
@@ -920,9 +920,9 @@ class parseCSV {
             {
                 if('' === $this->input_encoding)
                 {
-                    $this->input_encoding = mb_detect_encoding( $data, array(
+                    @$this->input_encoding = mb_detect_encoding( $data, array(
                         'ASCII',
-                        'Windows-1251','Windows-1252','Windows-1254','Windows-1257','KOI8-R','KOI8-U',
+                        'Windows-1251','Windows-1252','Windows-1254','KOI8-R','KOI8-U',
                         'ISO-8859-1','ISO-8859-2','ISO-8859-3','ISO-8859-4','ISO-8859-5','ISO-8859-6','ISO-8859-7',
                         'ISO-8859-8','ISO-8859-9','ISO-8859-10','ISO-8859-13','ISO-8859-14','ISO-8859-15','ISO-8859-16',
                         'UTF-8','UTF-32','UTF-32BE','UTF-32LE','UTF-16','UTF-16BE','UTF-16LE'
